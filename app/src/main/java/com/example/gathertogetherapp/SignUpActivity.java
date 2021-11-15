97b82584721c0aa19eafddd0f05c966650b9c070
Gyanendra Yadav <gyanendra.yadav_cs19@gla.ac.in>
2021-11-15 17:44:05 +0530

    Created The Object of The EditText and Button, so that values can be assigned.

--- app/src/main/java/com/example/gathertogetherapp/SignUpActivity.java
+++ app/src/main/java/com/example/gathertogetherapp/SignUpActivity.java
@@ -3,8 +3,13 @@ package com.example.gathertogetherapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SignUpActivity extends AppCompatActivity {
+    EditText userName,emailBox,password,alreadyHaveAccount;
+    Button signup;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
