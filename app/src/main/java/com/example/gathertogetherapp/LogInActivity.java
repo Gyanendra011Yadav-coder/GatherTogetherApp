39388330def6f60413674a90bae978cce0cf773e
Gyanendra Yadav <gyanendra.yadav_cs19@gla.ac.in>
2021-11-15 13:55:57 +0530

    Added The Code to Start  The SignUp activity just after The LogIn .

--- app/src/main/java/com/example/gathertogetherapp/LogInActivity.java
+++ app/src/main/java/com/example/gathertogetherapp/LogInActivity.java
@@ -3,7 +3,9 @@ package com.example.gathertogetherapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -22,5 +24,13 @@ public class LogInActivity extends AppCompatActivity {
         password=findViewById(R.id.password);
         loginBtn=findViewById(R.id.loginBtn);
         signUp=findViewById(R.id.signUp);
+
+        //Setting The Procedure Of Opening The SignUp Button just after The LogIn Screen
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LogInActivity.this,SignUpActivity.class));
+            }
+        });
     }
 }
