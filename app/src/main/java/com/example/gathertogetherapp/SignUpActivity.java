99a4c47ad55aed3b4d25128e13e9b0da851054c5
Gyanendra Yadav <gyanendra.yadav_cs19@gla.ac.in>
2021-11-15 17:49:37 +0530

    Assigned the Values to the Object Created Of TextView and Button.

--- app/src/main/java/com/example/gathertogetherapp/SignUpActivity.java
+++ app/src/main/java/com/example/gathertogetherapp/SignUpActivity.java
@@ -2,6 +2,7 @@ package com.example.gathertogetherapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,9 +12,18 @@ public class SignUpActivity extends AppCompatActivity {
     Button signup;
 
 
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        //Assigning The Value To the Objects That are Created Above
+        userName=findViewById(R.id.inputUserName);
+        emailBox=findViewById(R.id.inputEmail);
+        password=findViewById(R.id.inputPassword);
+        alreadyHaveAccount=findViewById(R.id.alreadyHaveAccount);
+        signup=findViewById(R.id.submitButton);
+
     }
 }
