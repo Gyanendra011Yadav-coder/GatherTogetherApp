35b9c7129466246b5c6bac69559085208477f183
Gyanendra Yadav <gyanendra.yadav_cs19@gla.ac.in>
2021-11-15 13:28:14 +0530

    Added the Comments, also have assigned the Values to the Created Objects of EditText,and the buttons.

--- app/src/main/java/com/example/gathertogetherapp/LogInActivity.java
+++ app/src/main/java/com/example/gathertogetherapp/LogInActivity.java
@@ -2,17 +2,25 @@ package com.example.gathertogetherapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class LogInActivity extends AppCompatActivity {
-    EditText emailBox,password;
-    Button loginBtn,signUpBtn;
+//Creating the Object Of Text-View and Button
+    EditText emailBox,password,signUp; // here SignUp is not used as an Button but it is used as TextView
+    Button loginBtn;
 
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+        //Assigning The Value To the Objects That are Created Above
+        emailBox=findViewById(R.id.emailBox);
+        password=findViewById(R.id.password);
+        loginBtn=findViewById(R.id.loginBtn);
+        signUp=findViewById(R.id.signUp);
     }
 }
