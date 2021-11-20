c47bfb3d61e1aa04b742567b924f9a38e5ac6bde
Gyanendra Yadav <gyanendra.yadav_cs19@gla.ac.in>
2021-11-20 11:45:32 +0530

    Passed the values to the Variable that have been Created before.And also C0orrectd the name of the Variables that was intialized.

--- app/src/main/java/com/example/gathertogetherapp/SignUpActivity.java
+++ app/src/main/java/com/example/gathertogetherapp/SignUpActivity.java
@@ -4,11 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class SignUpActivity extends AppCompatActivity {
-    EditText userName,emailBox,password,alreadyHaveAccount;
+    EditText userNameBox,emailBox,passwordBox,alreadyHaveAccount;
     Button signup;
 
 
@@ -19,11 +20,22 @@ public class SignUpActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sign_up);
 
         //Assigning The Value To the Objects That are Created Above
-        userName=findViewById(R.id.inputUserName);
+        userNameBox =findViewById(R.id.inputUserName);
         emailBox=findViewById(R.id.inputEmail);
-        password=findViewById(R.id.inputPassword);
+        passwordBox=findViewById(R.id.inputPassword);
         alreadyHaveAccount=findViewById(R.id.alreadyHaveAccount);
         signup=findViewById(R.id.submitButton);
 
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name,email,pass;
+                name = userNameBox.getText().toString();
+                email=emailBox.getText().toString();
+                pass=passwordBox.getText().toString();
+
+
+            }
+        });
+
     }
-}
