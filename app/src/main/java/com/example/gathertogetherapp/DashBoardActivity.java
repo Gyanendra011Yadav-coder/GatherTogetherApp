34738b4e31bb0474ddae5b7323db2519c77c3bb5
Gyanendra Yadav <gyanendra.yadav_cs19@gla.ac.in>
2021-11-24 21:50:41 +0530

    Creating the Variables and Passing The Value to them.

--- app/src/main/java/com/example/gathertogetherapp/DashBoardActivity.java
+++ app/src/main/java/com/example/gathertogetherapp/DashBoardActivity.java
@@ -3,12 +3,22 @@ package com.example.gathertogetherapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class DashBoardActivity extends AppCompatActivity {
+
+    EditText codeGenerateBox;
+    Button createMeetButton,shareButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash_board);
+
+        //Assigning The Value To The Variables Declared Above
+        codeGenerateBox=findViewById(R.id.codeBox);
+        createMeetButton=findViewById(R.id.createMeet);
+        shareButton=findViewById(R.id.shareButton);
     }
 }
