c61f0fe1b4c787c5c39b2983b0acc1fb5b3e718f
Gyanendra Yadav <gyanendra.yadav_cs19@gla.ac.in>
2021-11-24 21:52:39 +0530

    Setting The Click Listner To The Button.

--- app/src/main/java/com/example/gathertogetherapp/DashBoardActivity.java
+++ app/src/main/java/com/example/gathertogetherapp/DashBoardActivity.java
@@ -3,13 +3,16 @@ package com.example.gathertogetherapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import java.net.URL;
 
 public class DashBoardActivity extends AppCompatActivity {
 
     EditText codeGenerateBox;
-    Button createMeetButton,shareButton;
+    Button joinButton,shareButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +21,17 @@ public class DashBoardActivity extends AppCompatActivity {
 
         //Assigning The Value To The Variables Declared Above
         codeGenerateBox=findViewById(R.id.codeBox);
-        createMeetButton=findViewById(R.id.createMeet);
+        joinButton =findViewById(R.id.createMeet);
         shareButton=findViewById(R.id.shareButton);
+
+
+
+        joinButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
